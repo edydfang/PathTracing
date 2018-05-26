582369f08d068b4aed36735318cf20ee9181f99a
edwardfang <fangyd1997@outlook.com>
2018-05-26 20:54:57 +0800

    init version from smallpt

---
+++ imageio.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#pragma region
+
+#include "vector.hpp"
+
+#pragma endregion
+
+//-----------------------------------------------------------------------------
+// System Includes
+//-----------------------------------------------------------------------------
+#pragma region
+
+#define __STDC_LIB_EXT1__
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdio.h>
+
+#pragma endregion
+
+//-----------------------------------------------------------------------------
+// Declarations and Definitions
+//-----------------------------------------------------------------------------
+namespace smallpt
+{
+
+inline void WritePPM(uint32_t w, uint32_t h,
+					 const Vector3 *Ls, const char *fname = "./cpp-image.ppm") noexcept
+{
+
+	FILE *fp;
+
+#ifdef __WIN32__
+	fopen_s(&fp, fname, "w");
+#else
+	fp = fopen(fname, "w");
+	if (!fp)
+	{
+		perror("File opening failed");
+	}
+#endif
+
+	fprintf(fp, "P3\n%u %u\n%u\n", w, h, 255u);
+	for (size_t i = 0; i < w * h; ++i)
+	{
+		fprintf(fp, "%u %u %u ", ToByte(Ls[i].m_x),
+				ToByte(Ls[i].m_y),
+				ToByte(Ls[i].m_z));
+	}
+
+	fclose(fp);
+}
+} // namespace smallpt
---
+++ rayTracing.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifdef __WIN32__
+#include "targetver.h"
+#endif
+
+#include "sampling.hpp"
+#include "sphere.hpp"
+#include "specular.hpp"
+#include "imageio.hpp"
